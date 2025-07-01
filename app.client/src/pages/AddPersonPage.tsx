829fa6ccc01b29db9929ce585e02556868e1ca40
Lee McLafferty <lee.mclafferty@gmail.com>
2025-06-30 23:00:46 -0700

    Front end and backend connection methods for AddPersonPage

--- app.client/src/pages/AddPersonPage.tsx
+++ app.client/src/pages/AddPersonPage.tsx
@@ -1,8 +1,42 @@
+import type React from "react";
+
 const AddPersonPage = () => {
+
+    const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        fetch('http://localhost:7034/api/PersonRecord/Create', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(collectFormData(e.currentTarget))
+        })
+    }
+
+    const collectFormData = (form: HTMLFormElement) => {
+        const personRecord = {
+            firstName: (form.elements.namedItem("FirstName") as HTMLInputElement)?.value,
+            middleName: (form.elements.namedItem("MiddleName") as HTMLInputElement)?.value,
+            lastName: (form.elements.namedItem("LastName") as HTMLInputElement)?.value,
+            preferredName: (form.elements.namedItem("PreferredName") as HTMLInputElement)?.value,
+            Intials: (form.elements.namedItem("Intials") as HTMLInputElement)?.value,
+            startDate: (form.elements.namedItem("StartDate") as HTMLInputElement)?.value,
+            phoneNumber: (form.elements.namedItem("PhoneNumber") as HTMLInputElement)?.value,
+            deskNumber: (form.elements.namedItem("DeskNumber") as HTMLInputElement)?.value,
+            emailAddress: (form.elements.namedItem("EmailAddress") as HTMLInputElement)?.value,
+            isFullyRemote: (form.elements.namedItem("FullyRemote") as HTMLInputElement)?.checked,
+            jobTitle: (form.elements.namedItem("JobTitle") as HTMLInputElement)?.value,
+            jobLevel: (form.elements.namedItem("JobLevel") as HTMLInputElement)?.value,
+            department: (form.elements.namedItem("Department") as HTMLInputElement)?.value
+        };
+
+        return personRecord;
+    }
+
     return (
       <>
       <h2>Add Person Page</h2>
-      <form>
+            <form onSubmit={handleSubmit}>
         <label>
             First Name:
             <input type="text" name="FirstName" />
@@ -20,8 +54,8 @@ const AddPersonPage = () => {
             <input type="text" name="PreferredName" />
         </label>
         <label>
-            Intials:
-            <input type="text" name="Intials" />
+            initials:
+            <input type="text" name="initials" />
         </label>
         <label>
             Start Date:
@@ -29,7 +63,7 @@ const AddPersonPage = () => {
         </label>
         <label>
             Email:
-            <input type="email" name="Email" />
+            <input type="email" name="EmailAddress" />
         </label>
         <label>
             Phone Number:
@@ -55,8 +89,8 @@ const AddPersonPage = () => {
             Department:
             <input type="text" name="Department" />
         </label>
-        <input type="submit" value="submit"></input>
-      </form>
+         <input type="submit" value="submit"></input>
+    </form>
       </>
   );
 }
