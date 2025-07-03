3c598c4fd2a62f94294ffb41325cf4c899abbcbf
Lee McLafferty <lee.mclafferty@gmail.com>
2025-07-03 12:26:36 -0700

    Table formatting corretly for displaying users.

--- app.client/src/components/personRecordList/PersonRecordList.tsx
+++ app.client/src/components/personRecordList/PersonRecordList.tsx
@@ -23,9 +23,36 @@ const PersonRecordList = () => {
         return people.map(person => ({
             ...person,
             startDate: person.startDate ? new Date(person.startDate).toLocaleDateString() : "",
-            endDate: person.startDate ? new Date(person.endDate).toLocaleDateString() : "",
+            endDate: person.endDate ? new Date(person.endDate).toLocaleDateString() : "",
             isFullyRemote: person.isFullyRemote ? "Remote" : "In Office"
         }));
+    }
+
+    const getDisplayName = (person: any) => {
+        let first: string = "";
+        let last: string = "";
+        if (person.preferredName) {
+            // if preferredName has spaces, hinting at first and last name.
+            if (person.preferredName.includes(" ")) {
+                const names = person.preferredName.split(" ");
+                if (names) {
+                    first = names[0];
+                    for (let i = 1; i < names.length; i++) {
+                        if (i == 1) {
+                            last += names[i];
+                            continue;
+                        }
+                        last += (" " + names[i]);
+                    }
+                    return first + " " + last;
+                }
+            }
+            else {
+                return person.preferredName + " " + person.lastName;
+            }
+        } else {
+            return person.firstName + " " + person.lastName;
+        }
     }
 
     return (
@@ -35,16 +62,32 @@ const PersonRecordList = () => {
             <table>
                 <thead>
                     <tr>
-                        {Object.keys(personRecords[0]).map((key) => (
-                            <th key={key}>{formatHeader(key)}</th>
-                        ))}
+                        <th key="displayName">Display Name</th>
+                        {
+                            Object.keys(personRecords[0])
+                                .filter((key) => !key.toLowerCase().includes("name") && key != "id")
+                                .map((key) => (
+                                <th key={key}>{formatHeader(key)}</th>
+                            ))
+                        }
                     </tr>
                 </thead>
                 <tbody>
                     {formatPersonData(personRecords).map((person, i) => (
                         <tr key={i}>
-                            {Object.keys(person).map((key) => (
-                                <td key={key}>{person[key as keyof typeof person]?.toString()}</td>
+                            <td>
+                                <a href={`/editperson/${person.id}`}>
+                                    {getDisplayName(person)}
+                                </a>
+                            </td>
+                            {Object.keys(person)
+                                .filter((key) => !key.toLowerCase().includes("name") && key != "id")
+                                .map((key) => (
+                                <td key={key}>
+                                    {
+                                        person[key as keyof typeof person]?.toString()
+                                    }
+                                </td>
                             ))}
                         </tr>
                     ))}
