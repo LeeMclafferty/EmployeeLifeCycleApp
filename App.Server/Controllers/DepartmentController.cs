5ce75c5080e1a33ffe80d037ba82c3528d41ae09
Lee McLafferty <lee.mclafferty@gmail.com>
2025-07-29 17:32:51 -0700

    Contrller for fetching team and department data complete

---
+++ App.Server/Controllers/DepartmentController.cs
@@ -0,0 +1,48 @@
+﻿using App.Server.Data;
+using App.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace App.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public DepartmentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Get/{id}")]
+        public async Task<ActionResult<Department>> GetById(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+
+            if (department == null)
+                return NotFound(new { message = "Department not found" });
+
+            return Ok(department);
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var departments = await _context.Departments.ToListAsync();
+
+                if (!departments.Any())
+                    return NotFound(new { message = "Unable to find any departments" });
+
+                return Ok(departments);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = $"Server error: {ex.Message}", stackTrace = ex.StackTrace });
+            }
+        }
+    }
+}
---
+++ App.Server/Controllers/TeamController.cs
@@ -0,0 +1,46 @@
+﻿using App.Server.Data;
+using App.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public TeamController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("Get/{id}")]
+        public async Task<ActionResult<Team>> GetById(int id)
+        {
+            var team = await _context.Teams
+                .Include(t => t.Department)
+                .FirstOrDefaultAsync(t => t.Id == id);
+
+            if (team == null)
+                return NotFound(new { message = "Team not found." });
+
+            return Ok(team);
+        }
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> GetAll()
+        {
+            var teams = await _context.Teams
+                .Include(t => t.Department)
+                .ToListAsync();
+
+            if(!teams.Any())
+            {
+                return NotFound(new {message = "Unable to find any teams."});
+            }
+
+            return Ok(teams);
+        }
+    }
+}
